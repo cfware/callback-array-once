ff21fd310461d89531444ab9993dca388ffde2a9
Corey Farrell <git@cfware.com>
2023-10-15 11:17:10 -0400

    fix: Update development dependencies

--- callback-array-once.js
+++ callback-array-once.js
@@ -1,5 +1,5 @@
 export default (list, ...args) => {
-	for (const fn of list.splice(0)) {
-		fn(...args);
-	}
+    for (const fn of list.splice(0)) {
+        fn(...args);
+    }
 };
--- test.js
+++ test.js
@@ -3,50 +3,50 @@ import t from 'libtap';
 import runCallbacks from '@cfware/callback-array-once';
 
 t.test('exports a function', async t => {
-	t.type(runCallbacks, 'function');
+    t.type(runCallbacks, 'function');
 });
 
 t.test('first argument must be iterable', async t => {
-	t.throws(() => runCallbacks());
+    t.throws(() => runCallbacks());
 });
 
 t.test('empty array', async () => runCallbacks([]));
 
 t.test('callback order', async t => {
-	const track = {
-		first: false,
-		second: false
-	};
-	const after = () => {};
-	const cbs = [
-		() => {
-			t.same(track, {first: false, second: false});
-			track.first = true;
-		},
-		() => {
-			t.same(track, {first: true, second: false});
-			track.second = true;
-		},
-		() => {
-			t.same(track, {first: true, second: true});
-			cbs.push(after);
-		}
-	];
+    const track = {
+        first: false,
+        second: false
+    };
+    const after = () => {};
+    const cbs = [
+        () => {
+            t.same(track, {first: false, second: false});
+            track.first = true;
+        },
+        () => {
+            t.same(track, {first: true, second: false});
+            track.second = true;
+        },
+        () => {
+            t.same(track, {first: true, second: true});
+            cbs.push(after);
+        }
+    ];
 
-	runCallbacks(cbs);
-	t.same(track, {first: true, second: true});
-	t.equal(cbs.length, 1);
-	t.equal(cbs[0], after);
+    runCallbacks(cbs);
+    t.same(track, {first: true, second: true});
+    t.equal(cbs.length, 1);
+    t.equal(cbs[0], after);
 });
 
 t.test('arguments', async t => {
-	const cbs = [
-		(a, b, c) => {
-			t.equal(a, 'a');
-			t.equal(b, 'b');
-			t.equal(c, 'c');
-		}
-	];
+    const cbs = [
+        (a, b, c) => {
+            t.equal(a, 'a');
+            t.equal(b, 'b');
+            t.equal(c, 'c');
+        }
+    ];
 
-	runCallbacks(cbs, 'a', 'b', 'c');
+    runCallbacks(cbs, 'a', 'b', 'c');
 });
